0d719af9850041abac6298a34d7cfc8ad68f1287
Amit Saini <amitsaini.ce@gmail.com>
2026-04-16 18:00:31 +0530

    Add client and server-side validation using Joi, implement centralized error handling, and async wrapper middleware

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-const req = require("express/lib/request.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+//To handle Error
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+//For server side validation
+const { listingSchema } = require("./schema.js");
 
 app.engine("ejs", ejsMate);
 
@@ -14,6 +18,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(methodOverride('_method'));
 
@@ -24,6 +29,9 @@ let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
     .then(() => {
         console.log("Database connected successfully");
+        app.listen(port, () => {
+            console.log(`Server is listing to port: ${port}`);
+        })
     })
     .catch((err) => {
         console.log(err);
@@ -33,15 +41,26 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        return next (new ExpressError(400, errMsg));
+    }
+    else {
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Hiii I am root");
 });
 
 //To see all Listings
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 //To display for to add new listing
 app.get("/listings/new", (req, res) => {
@@ -49,41 +68,50 @@ app.get("/listings/new", (req, res) => {
 });
 
 //To see a listing in detail
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-});
+}));
 
 //To add new listing in DB
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     const newListing = await new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 //Form to edit Listing
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
+}));
 //To update edited data in DB
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let listing = req.body.listing;
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, listing);
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //To delete a listing
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     console.log(id);
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+//404 handler
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
 });
 
-app.listen(port, () => {
-    console.log(`Server is listing to port: ${port}`);
-})
+//Error handling middleware
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).render("listings/error.ejs", { message });
+});
+
+
--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,7 @@ const listingSchema = new Schema({
     },
     image: {
         type: String,
-        set: (v) => v === "https://plus.unsplash.com/premium_photo-1684175656320-5c3f701c082c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ? link : v,
+        // set: (v) => v === "https://plus.unsplash.com/premium_photo-1684175656320-5c3f701c082c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ? link : v,
         default: "https://plus.unsplash.com/premium_photo-1684175656320-5c3f701c082c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     price: {
